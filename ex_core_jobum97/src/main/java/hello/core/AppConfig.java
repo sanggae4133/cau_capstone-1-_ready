f28b7ee6f90c9654c4837917f5e80a25426acd40
jobum97 <brian654321@naver.com>
2021-08-05 20:48:34 +0900

    스프링 빈 조회 까지

---
+++ ex_core_jobum97/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,39 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() { //역활
+        return new MemberServiceImpl(memberRepository()); //구현
+    }
+
+    @Bean
+    public MemberRepository memberRepository() { //역활
+        return new MemoryMemberRepository(); //구현
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy() { //역활
+        //return new FixDiscountPolicy(); //구현
+        return new RateDiscountPolicy(); //구현체 갈아끼우기
+    }
+
+    @Bean
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+}
